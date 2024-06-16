bc939aba50521912746cc0a102ee0c4a558c28b4
avideb24 <debavi24@gmail.com>
2024-06-16 18:22:41 +0600

    updated Header component and clear unsual codes from Sidebar

--- src/components/Shared/Header/Header.jsx
+++ src/components/Shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { CiSearch } from "react-icons/ci";
 
 
-const Header = ({title}) => {
+const Header = ({ title }) => {
 
     const handleSearchCustomer = e => {
         const searchText = e.target.value;
@@ -11,20 +11,22 @@ const Header = ({title}) => {
 
 
     return (
-        <div className='sticky left-0 top-0 p-4 bg-white ml-10 lg:ml-0'>
-            
-           <div className='flex justify-between gap-2'>
-           {
-                title == 'home' ?
-                <form className='max-w-sm relative'>
-                    <input type="text" onChange={(e) => handleSearchCustomer(e)} className='w-full pl-6 pr-2 py-1 border-2 border-slate-300 rounded-md outline-none' placeholder='Search Customer...' />
-                    <span className='absolute left-2 top-[10px] text-slate-600'><CiSearch /></span>
-                </form>
-                :
-                <h1>{title}</h1>
-            }
-            <button className='bg-primary-color text-white font-semibold px-3 py-1 rounded-md'>+ Add New</button>
-           </div>
+        <div className='sticky left-0 top-0 p-4 bg-white shadow-md'>
+
+            <div className='pl-10 lg:pl-0'>
+                <div className='flex justify-between gap-2'>
+                    {
+                        title == 'home' ?
+                            <form className='max-w-sm relative'>
+                                <input type="text" onChange={(e) => handleSearchCustomer(e)} className='w-full pl-6 pr-2 py-1 border-2 border-slate-300 rounded-md outline-none' placeholder='Search Customer...' />
+                                <span className='absolute left-2 top-[10px] text-slate-600'><CiSearch /></span>
+                            </form>
+                            :
+                            <h1>{title}</h1>
+                    }
+                    <button className='bg-primary-color text-white font-semibold px-3 py-1 rounded-md'>+ Add New</button>
+                </div>
+            </div>
         </div>
     );
 };
