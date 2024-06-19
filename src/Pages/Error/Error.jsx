91a4d333cc48ec7018d3d51bf51572aa6de43afa
avideb24 <debavi24@gmail.com>
2024-06-19 15:37:51 +0600

    added errorPage

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router-dom';
+import errorImg from '../../assets/error.png';
 
 const Error = () => {
     return (
-        <div>
-            error
+        <div className='w-full h-screen flex flex-col items-center justify-center'>
+            <div className='max-w-md'>
+                <img src={errorImg} className='w-full object-contain' alt="Error Image" />
+            </div>
+            <h3 className='text-base md:text-xl font-bold mb-5'>Sorry! Your Requested page was not found!</h3>
+            <Link to={'/'} className='bg-[#5fc3d1] text-white font-semibold px-4 py-2 rounded-sm'>Go Home</Link>
         </div>
     );
 };
